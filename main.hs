45f30d8dc1f12d4c15964c9d6670f11dadbaec71
Tomasz Gieniusz <tomasz.gieniusz@ihs.com>
2015-10-20 11:26:09 +0200

    identifying full house

--- main.hs
+++ main.hs
@@ -6,7 +6,7 @@ data Suit = Hearts | Diamonds | Spades | Clubs deriving (Show, Eq, Ord)
 data Rank = A | K | Q | J | Ten | Nine | Eight | Seven | Six | Five | Four | Three | Two deriving (Show, Eq, Ord)  
 data Card = Card {rank :: Rank, suit :: Suit} deriving (Show, Eq, Ord)  
 type Kicker = Rank
-data Hand = FourOfAKind (Rank) |ThreeOfAKind (Rank) | TwoPairs (Rank, Rank) | Pair Rank | HighestCard Rank deriving (Show, Eq, Ord)  
+data Hand = FullHouse (Rank, Rank) | FourOfAKind (Rank) |ThreeOfAKind (Rank) | TwoPairs (Rank, Rank) | Pair Rank | HighestCard Rank deriving (Show, Eq, Ord)  
 
 groupDuplicates :: Eq a => [a] -> [[a]]
 groupDuplicates [] = []
@@ -33,20 +33,22 @@ highestCard = Just . HighestCard . rank . head . sort
 pair :: [Card] -> Maybe Hand
 pair cards = (single . (rankGroupsOf 2) $ cards) >>= (Just . Pair)
 
-twoPairs :: [Card] -> Maybe Hand
-twoPairs cards = (double . (rankGroupsOf 2) $ cards) >>= (Just . TwoPairs)
-
 threeOfAKind :: [Card] -> Maybe Hand
 threeOfAKind cards = (single . (rankGroupsOf 3) $ cards) >>= (Just . ThreeOfAKind)
 
 fourOfAKind :: [Card] -> Maybe Hand
 fourOfAKind cards = (single . (rankGroupsOf 4) $ cards) >>= (Just . FourOfAKind)
 
+twoPairs :: [Card] -> Maybe Hand
+twoPairs cards = (double . (rankGroupsOf 2) $ cards) >>= (Just . TwoPairs)
+
+fullHouse :: [Card] -> Maybe Hand
+fullHouse cards = (single (zip (rankGroupsOf 3 cards) (rankGroupsOf 2 cards))) >>= (Just . FullHouse)
 
 identifyHand :: [Card] -> Hand
 identifyHand cards = head . sort $ hands
-	where hands = catMaybes $ map ($ cards) [highestCard, pair, twoPairs, threeOfAKind, fourOfAKind]
+	where hands = catMaybes $ map ($ cards) [highestCard, pair, twoPairs, threeOfAKind, fourOfAKind, fullHouse]
 
 main = do 
-	let cards = [(Card A Hearts), (Card A Diamonds), (Card A Clubs), (Card A Spades)]
+	let cards = [(Card A Hearts), (Card A Diamonds), (Card A Clubs), (Card K Spades), (Card K Clubs)]
 	print $ identifyHand cards
